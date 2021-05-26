959ee8cf6400949793d7b43352e2afafe4f3bce7
varekaiwork <serhiisaviukwork@gmail.com>
2021-05-26 17:06:10 +0300

    document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
+import Head from 'next/head'
 import { AppProps } from "next/dist/next-server/lib/router/router";
+
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
-}
+  return (
+    <>
+    <Head>
+      <title>MyTop</title>
+      <link key={1} rel="icon" href="/favicon.ico"/>
+    </Head>
+    <Component {...pageProps} />;
+    </>
+  
+  )}
+
 
 export default MyApp;
